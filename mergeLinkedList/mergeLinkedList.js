3eb6b7682f0157fe50b287a06352cdb246d4ecc7
Thomas Johnson <thomasij813@gmail.com>
2019-12-16 15:05:25 -0800

    Add test for mergeLinkedLists

---
+++ mergeLinkedList/mergeLinkedList.js
@@ -0,0 +1,18 @@
+/* 
+Merge two Linked Lists at alternating positions
+Objective: Given two linked lists, merge one list into another at alternate
+positions, if second link list has extra nodes, print them as well
+
+Example:
+5 -> 10 -> 15 -> 20 ->25 -> null
+3 -> 6 ->9 -> 12 ->15 ->18 ->21 -> null
+
+Output :
+5 -> 3 -> 10 -> 6 ->15 -> 9 -> 20 -> 12 -> 25 ->15 -> null
+
+Remaining List : 18 -> 21 -> null
+*/
+
+const mergeLinkedLists = (list1, list2) => {};
+
+module.exports = mergeLinkedLists;
---
+++ mergeLinkedList/mergeLinkedList.test.js
@@ -0,0 +1,47 @@
+const mergeLinkedLists = require("./mergeLinkedList.js");
+
+const ListNode = function(val) {
+  this.val = val;
+  this.next = null;
+};
+
+const linkTheList = vals => {
+  let tail = null;
+  let head = null;
+
+  vals.forEach(val => {
+    const node = new ListNode(val);
+    if (!head) {
+      head = node;
+      tail = node;
+    } else {
+      tail.next = node;
+      tail = node;
+    }
+  });
+
+  return head;
+};
+
+const forEachNode = (node, callback) => {
+  callback(node.val);
+
+  if (node.next) {
+    forEachNode(node.next, callback);
+  }
+};
+
+const mapLinkedList = node => {
+  const out = [];
+  forEachNode(node, val => out.push(val));
+  return out;
+};
+
+describe("mergeLinkedLists", () => {
+  it("Should merge two linked lists", () => {
+    ll1 = linkTheList([5, 10, 15, 20, 25]);
+    ll2 = linkTheList([3, 6, 9, 12, 15, 18, 21]);
+    expectedOutput = [5, 3, 10, 6, 15, 9, 20, 12, 25, 15];
+    expect(mapLinkedList(mergeLinkedLists(ll1, ll2))).toEqual(expectedOutput);
+  });
+});
