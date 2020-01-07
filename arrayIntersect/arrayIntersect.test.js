e4fc0100a8b311e0c0ee324727248e91a7e12600
Thomas Johnson <thomasij813@gmail.com>
2020-01-06 16:40:20 -0800

    Update tests for arrayIntersect to accurately reflect problem

--- arrayIntersect/arrayIntersect.test.js
+++ arrayIntersect/arrayIntersect.test.js
@@ -1,9 +1,23 @@
 const arrayIntersect = require("./arrayIntersect.js");
 
 describe("arrayIntersect", () => {
-  it("Should find the first intersection between two arrays", () => {
+  it("Should find the intersection between two arrays", () => {
     const arr1 = [4, 9, 5];
     const arr2 = [9, 4, 9, 8, 4];
-    expect(arrayIntersect(arr1, arr2)).toEqual([4, 9]);
+    let output = arrayIntersect(arr1, arr2);
+    expect(output.length).toBe(2);
+    output.forEach(val => {
+      expect([4, 9]).toContain(val);
+    });
+  });
+
+  it("Should include as many results as there are shared numbers", () => {
+    const arr1 = [1, 2, 2, 1];
+    const arr2 = [2, 2];
+    let output = arrayIntersect(arr1, arr2);
+    expect(output.length).toBe(2);
+    output.forEach(val => {
+      expect([2, 2]).toContain(val);
+    });
   });
 });
