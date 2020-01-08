56c75e2f46b30c9f4984bb824158b99cf617c739
Thomas Johnson <thomasij813@gmail.com>
2020-01-07 17:41:15 -0800

    Test for singleNumber

---
+++ singleNumber/singleNumber.test.js
@@ -0,0 +1,7 @@
+const singleNumber = require("./singleNumber.js");
+
+describe("singleNumber", () => {
+  it("Should find the single number in an array", () => {
+    expect(singleNumber(4, 1, 2, 1, 2)).toBe(4);
+  });
+});
